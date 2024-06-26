89aac5ef29b0c299b6c58e062d72e8733a29884d
Алекасндр Силантьев <sawkasilan@gmail.com>
2024-06-26 16:57:50 +0300

    дз 2.31

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,30 @@
 function checkStringLength(string, maxLength) {
-  if (string <= maxLength) {
+  if (string.length <= maxLength) {
     return true;
   } else {
     return false;
   }
 }
 
-// Строка короче 20 символов
+
 checkStringLength('проверяемая строка', 20); // true
-// Длина строки ровно 18 символов
 checkStringLength('проверяемая строка', 18); // true
-// Строка длиннее 10 символов
 checkStringLength('проверяемая строка', 10); // false
+
+function isPalidrom(string) {
+  string = string.toLowerCase();
+  string = string.replaceAll(' ', '');
+
+  let reversed = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversed = reversed + string[i];
+  }
+
+  return string === reversed;
+}
+
+
+isPalidrom('топот'); // true
+isPalidrom('ДовОд'); // true
+isPalidrom('Кекс'); // false
